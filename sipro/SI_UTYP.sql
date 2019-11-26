76fe1e122ee3bf5de1b51d23270ceb9dae7db9f2
nhilo32 <nhilo32@yahoo.com>
2019-11-26 01:55:39 -0500

    Delete SI_UTYP.sql

--- sipro/SI_UTYP.sql
+++
@@ -1,9 +0,0 @@
---------------------------------------------------------
---  DDL for Table SI_UTYP
---------------------------------------------------------
-
-  CREATE TABLE "SIPRO"."SI_UTYP" 
-   (	"TYPE_CODE" VARCHAR2(26), 
-	"TYPE_DESCR" VARCHAR2(26), 
-	"TYPE_SHORT" VARCHAR2(26)
-   ) ;
