fd73f98438e21bfd64c588142072dae8694bc4e0
nhilo32 <nhilo32@yahoo.com>
2019-11-26 01:54:43 -0500

    Delete STDVIEW.sql

--- sipro/STDVIEW.sql
+++
@@ -1,9 +0,0 @@
---------------------------------------------------------
---  DDL for Table STDVIEW
---------------------------------------------------------
-
-  CREATE TABLE "SIPRO"."STDVIEW" 
-   (	"RPTID" NUMBER(38,0), 
-	"FLD_ORDER" NUMBER(38,0), 
-	"FIELD_NAME" VARCHAR2(26)
-   ) ;
