778d150ebdb32e589ac24cf750fcaebbbc2e1450
nhilo32 <nhilo32@yahoo.com>
2019-11-26 01:59:29 -0500

    Delete SI_EXCHG.sql

--- sipro/SI_EXCHG.sql
+++
@@ -1,8 +0,0 @@
---------------------------------------------------------
---  DDL for Table SI_EXCHG
---------------------------------------------------------
-
-  CREATE TABLE "SIPRO"."SI_EXCHG" 
-   (	"EXCHG_CODE" VARCHAR2(26), 
-	"EXCHG_DESC" VARCHAR2(26)
-   ) ;
