824863288983e8bcd27f82f54e6f19a62a981896
nhilo32 <nhilo32@yahoo.com>
2019-11-26 02:01:46 -0500

    Delete SI_PTYP.sql

--- sipro/SI_PTYP.sql
+++
@@ -1,8 +0,0 @@
---------------------------------------------------------
---  DDL for Table SI_PTYP
---------------------------------------------------------
-
-  CREATE TABLE "SIPRO"."SI_PTYP" 
-   (	"TYPE_CODE" VARCHAR2(26), 
-	"TYPE_DESCR" VARCHAR2(26)
-   ) ;
